b84094a3a791bff31fcd279f6f7a68386a687d95
Onur Tanrıkulu <onurtnrkl@gmail.com>
2018-04-08 17:19:38 +0200

    Map style updated.

--- js/map.js
+++ js/map.js
@@ -1,32 +1,47 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3VsZWJlcnJhIiwiYSI6ImNqN2tyNWE5ejJkNTQzM250bjlhNDU1cGUifQ.gG1gUGCN16JvZnKFvxZTUg';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/suleberra/cjex5amaz03382ro4webnul0h', // stylesheet location
-    center: [4.625303494062859,52.43370081519061], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    style: 'mapbox://styles/suleberra/cjfqr6rn1031a2srp70x7pgz5', // stylesheet location
+    center: [4.89228829624804, 52.37710810531087], // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
 
-map.on('load', function () 
-{
-    // Add a symbol layer.
+map.on('load', function () {
     map.addLayer(
-    {
-        "id": "hotels",
-        "type": "symbol",
-        "source": 
         {
-            "type": "geojson",
-            "data": 'https://rawgit.com/suleberra/mapbox/master/data/hotel_deneme.json'
-        },
-        "layout": 
+            "id": "interventions",
+            "type": "circle",
+            "minzoom": 10,
+            "source":
+                {
+                    "type": "geojson",
+                    "data": 'https://rawgit.com/suleberra/mapbox/master/data/hotel_deneme.json'
+                },
+            "layout":
+                {
+                    "icon-image": "rocket-15"
+                }
+        });
+
+    map.addLayer(
         {
-            "icon-image": "rocket-15"
-        }
-    });
+            "id": "hotels",
+            "type": "symbol",
+            "minzoom": 10,
+            "source":
+                {
+                    "type": "geojson",
+                    "data": 'https://rawgit.com/suleberra/mapbox/master/data/hotel_deneme.json'
+                },
+            "layout":
+                {
+                    "icon-image": "rocket-15"
+                }
+        });
 
     // Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
     map.on('click', 'hotels', function (e) {
-        map.flyTo({center: e.features[0].geometry.coordinates});
+        map.flyTo({ center: e.features[0].geometry.coordinates });
         openNav(e.features[0].properties.name);
     });
 
